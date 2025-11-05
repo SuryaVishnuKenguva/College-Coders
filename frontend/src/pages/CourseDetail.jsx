38255de5ceb630de0ed54cd971d854c3ffac231f
Surya Vishnu <suryakenguva143@gmail.com>
2025-11-05 19:30:25 +0530

    Final fix of Course List

--- frontend/src/pages/CourseDetail.jsx
+++ frontend/src/pages/CourseDetail.jsx
@@ -5,7 +5,7 @@ import CourseInfoSection from "../components/CourseInfoSection";
 import CourseSidebar from "../components/CourseSidebar";
 import { courseData } from "../data/courseData";
 import { useParams } from "react-router-dom";
-import CoursePlaylist from "../components/CoursePlaylist";
+import CourseList from "../components/CourseList";
 
 export default function CourseDetail() {
   const { slug } = useParams();
@@ -66,7 +66,7 @@ export default function CourseDetail() {
           {/* Left Column: Course Details (takes 2 columns on large screens) */}
           <div className="lg:col-span-2 space-y-6 sm:space-y-8">
             <CourseInfoSection course={course} />
-            <CoursePlaylist videos={videos} loading={loading} error={error} />
+            <CourseList videos={videos} loading={loading} error={error} />
           </div>
 
           {/* Right Column: Sidebar (takes 1 column on large screens) */}
